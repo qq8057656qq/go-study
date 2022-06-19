70444807d5eb6432a795f1c8bd2d38f91588c460
Leoooooo <lvhuan2015@gmail.com>
2022-06-19 22:39:00 +0800

    refactor support test cases

--- reflect/walk.go
+++ reflect/walk.go
@@ -4,7 +4,8 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
-	//fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
