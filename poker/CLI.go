f593cbe3f662af78578c077fcc5803cb703a4695
Leoooooo <lvhuan2015@gmail.com>
2022-06-25 08:57:36 +0800

    handler win player with Scanner read

--- poker/CLI.go
+++ poker/CLI.go
@@ -1,9 +1,33 @@
 package poker
 
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
 type CLI struct {
 	playerStore PlayerStore
+	in          *bufio.Scanner
 }
 
-func (c CLI) PlayPoker() {
-	c.playerStore.RecordWin("lvhuan")
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+	}
+}
+
+func (c *CLI) PlayPoker() {
+	userInput := c.readLine()
+	c.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (c *CLI) readLine() string {
+	c.in.Scan()
+	return c.in.Text()
 }
--- poker/CLI_test.go
+++ poker/CLI_test.go
@@ -1,14 +1,36 @@
 package poker
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestCLI(t *testing.T) {
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
-	cli.PlayPoker()
-	if len(playerStore.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
+}
+
+func assertPlayerWin(t *testing.T, playerStore *StubPlayerStore, winner string) {
+	t.Helper()
+	if len(playerStore.winCalls) < 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(playerStore.winCalls), 1)
+	}
+	got := playerStore.winCalls[0]
+	if got != winner {
+		t.Errorf("didn't record correct winner,got '%s',want '%s'", got, winner)
 	}
 }
